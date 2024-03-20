87fe1e2500209de0313138f3a87e9a4e0cb54440
Ramon Dias <ramondiaz.ads@gmail.com>
2024-03-20 11:19:57 -0300

    adiciona estilo a nav bar

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit{
 
+  opened = false;
+  innerWidth: any;
+
+  ngOnInit(): void {
+    this.innerWidth = window.innerWidth;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(){
+    this.innerWidth = window.innerWidth;
+    if(this.innerWidth > 767 && this.opened === true){
+      this.opened = false;
+    }
+  }
+
+  toggleMenu(){
+    this.opened = !this.opened;
+  }
 }
