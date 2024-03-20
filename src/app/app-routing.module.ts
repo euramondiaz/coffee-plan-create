3866995043b522dd5281c366252879d20b3d78c3
Ramon Dias <ramondiaz.ads@gmail.com>
2024-03-20 10:57:22 -0300

    cria template da pagina home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { PlanComponent } from './components/plan/plan.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: HomePageComponent
+  },
+  {
+    path: 'about', component: AboutComponent
+  },
+  {
+    path: 'plan', component: PlanComponent
+  },
+  {
+    path: '**', redirectTo: ''
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
