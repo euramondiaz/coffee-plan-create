5cd742e5cbab8d3282efab595cc3c711d17b0161
Ramon Dias <ramondiaz.ads@gmail.com>
2024-03-20 10:51:37 -0300

    gera os components que serão utilizados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { PlanComponent } from './plan/plan.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    FooterComponent,
+    HomePageComponent,
+    NavBarComponent,
+    PlanComponent
   ],
   imports: [
     BrowserModule,
